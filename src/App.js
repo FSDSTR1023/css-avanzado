82c49d6431dadda726e0f809c312ad7e2ad65930
Miki <miquelxubet@gmail.com>
2023-10-19 00:13:51 +0200

    navbar implementation mxubet

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            <li>Product</li>
+            <li>Features</li>
+            <li>Marketplace</li>
+            <li>Company</li>
+            <li>Log in</li>
+          </ul>
+      </header>
+      <body>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,11 +22,10 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
+      </body>
     </div>
   );
 }
