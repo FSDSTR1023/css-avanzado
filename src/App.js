b306c2ade8fdbbf1a7135e4be7182e98188dc44a
joelmontilla <joel.montilla.heredia@gmail.com>
2023-10-18 23:42:03 +0200

    joel montilla ft/nav implementation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="container">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="links-container">
+          <ul>
+            <li>Product</li>
+            <li>Features</li>
+            <li>Marketplace</li>
+            <li>Company</li>
+            <li>Log in</li>
+          </ul>
+        </div>
+        
+      </div>        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
