166e2cc46bea4ddd880793cd0db574d6219cf290
alexander-lh <lasherasalexander@gmail.com>
2023-10-19 10:34:01 +0200

    feat: nav exercise

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className='nav-wrapper'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <ul className='nav-items'>
+            <li>Product</li>
+            <li>Features</li>
+            <li>Marketplaces</li>
+            <li>Company</li>
+            <li>Log in</li>
+            </ul>
+          </div>
+        </nav>
       </header>
     </div>
   );
