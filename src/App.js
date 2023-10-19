c06f946a760f5c851debcf765283be63363ed18a
Enrique Garcia <enrique@amigosdelcara.com>
2023-10-19 12:50:07 +0200

    creación de navegación del ejercicio 1

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <nav className="navbar">
+            <img src={logo} className="App-logo" alt="logo" />
+            <ul className="navitems">
+              <li>Product</li>
+              <li>Features</li>
+              <li>Marketplace</li>
+              <li>Company</li>
+              <li>Log in -&gt;</li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+      </main>
     </div>
   );
 }
