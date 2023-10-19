56a007a431ff3d6d1adb61bba8f8b0de23aab84a
Juan Mencía <juamm99@gmail.com>
2023-10-19 15:48:00 +0200

    Ejercicio 1

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* Ejercicio 1 */}
         <nav class="nav-wrapper">
-          
+          <div id="contenedor-logo-ej1">
+            <img src={logo} class="App-logo" id="logo-ej1" alt="logo" />
+          </div>
+          <ul id="lista-ej1">
+            <li>Product</li>
+            <li>Features</li>
+            <li>Marketplace</li>
+            <li>Company</li>
+            <li>Log in →</li>
+          </ul>
         </nav>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} class="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
