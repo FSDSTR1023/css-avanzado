998c3cc8380a552b8189c31c5021e8bb65708d78
tonebonedev <tonifcb@gmail.com>
2023-10-19 21:40:20 +0200

    changing tag for img class

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         {/* container for a card */}
         <div className="card">
           <img
-            alt="photo-of-jane"
+            alt="employee"
             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
           />
           <h3 className="name">Jane Cooper</h3>
@@ -44,7 +44,7 @@ function App() {
         </div>
         <div className="card">
           <img
-            alt="photo-of-jane"
+            alt="employee"
             src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
           />
           <h3 className="name">Cody Fisher</h3>
@@ -79,7 +79,7 @@ function App() {
         </div>
         <div className="card">
           <img
-            alt="photo-of-jane"
+            alt="employee"
             src="https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
           />
           <h3 className="name">Esther Howard</h3>
@@ -114,7 +114,7 @@ function App() {
         </div>
         <div className="card">
           <img
-            alt="photo-of-jane"
+            alt="employee"
             src="https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
           />
           <h3 className="name">Jenny Wilson</h3>
@@ -149,7 +149,7 @@ function App() {
         </div>
         <div className="card">
           <img
-            alt="photo-of-jane"
+            alt="employee"
             src="https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
           />
           <h3 className="name">Kristin Watson</h3>
@@ -184,7 +184,7 @@ function App() {
         </div>
         <div className="card">
           <img
-            alt="photo-of-jane"
+            alt="employee"
             src="https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
           />
           <h3 className="name">Cameron Williamson</h3>
@@ -219,7 +219,7 @@ function App() {
         </div>
         <div className="card">
           <img
-            alt="photo-of-jane"
+            alt="employee"
             src="https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
           />
           <h3 className="name">Courtney Henry</h3>
@@ -254,7 +254,7 @@ function App() {
         </div>
         <div className="card">
           <img
-            alt="photo-of-jane"
+            alt="employee"
             src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
           />
           <h3 className="name">Theresa Web</h3>
