71ab748152a1149efe9c3b346ebea4a3a42a0e00
alexander-lh <lasherasalexander@gmail.com>
2023-10-19 21:55:14 +0200

    feat: added card design exercise

--- src/App.js
+++ src/App.js
@@ -3,23 +3,170 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" /><body className="App">
       <header className="App-header">
         <nav className='nav-wrapper'>
           <img src={logo} className="App-logo" alt="logo" />
           <div>
             <ul className='nav-items'>
-            <li>Product</li>
-            <li>Features</li>
-            <li>Marketplaces</li>
-            <li>Company</li>
-            <li>Log in</li>
+              <li>Product</li>
+              <li>Features</li>
+              <li>Marketplaces</li>
+              <li>Company</li>
+              <li>Log in</li>
             </ul>
           </div>
         </nav>
       </header>
-    </div>
+      <main>
+        <section className='card-wrapper'>
+          <div className='card-info'>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330' alt='Personal'></img>
+            <h3>First name Second name</h3>
+            <p>Work position</p>
+            <p>Status</p>
+          </div>
+          <div className='card-contact'>
+            <div className='card-email'>
+            <span class="material-symbols-outlined">contact_mail</span>
+              <p>Email</p>
+            </div>
+            <div className='card-phone'>
+            <span class="material-symbols-outlined">call</span>
+              <p>Call</p>
+            </div>
+          </div>
+        </section>
+        <section className='card-wrapper'>
+          <div className='card-info'>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330' alt='Personal'></img>
+            <h3>First name Second name</h3>
+            <p>Work position</p>
+            <p>Status</p>
+          </div>
+          <div className='card-contact'>
+            <div className='card-email'>
+            <span class="material-symbols-outlined">contact_mail</span>
+              <p>Email</p>
+            </div>
+            <div className='card-phone'>
+            <span class="material-symbols-outlined">call</span>
+              <p>Call</p>
+            </div>
+          </div>
+        </section>
+        <section className='card-wrapper'>
+          <div className='card-info'>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330' alt='Personal'></img>
+            <h3>First name Second name</h3>
+            <p>Work position</p>
+            <p>Status</p>
+          </div>
+          <div className='card-contact'>
+            <div className='card-email'>
+            <span class="material-symbols-outlined">contact_mail</span>
+              <p>Email</p>
+            </div>
+            <div className='card-phone'>
+            <span class="material-symbols-outlined">call</span>
+              <p>Call</p>
+            </div>
+          </div>
+        </section>
+        <section className='card-wrapper'>
+          <div className='card-info'>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330' alt='Personal'></img>
+            <h3>First name Second name</h3>
+            <p>Work position</p>
+            <p>Status</p>
+          </div>
+          <div className='card-contact'>
+            <div className='card-email'>
+            <span class="material-symbols-outlined">contact_mail</span>
+              <p>Email</p>
+            </div>
+            <div className='card-phone'>
+            <span class="material-symbols-outlined">call</span>
+              <p>Call</p>
+            </div>
+          </div>
+        </section>
+        <section className='card-wrapper'>
+          <div className='card-info'>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330' alt='Personal'></img>
+            <h3>First name Second name</h3>
+            <p>Work position</p>
+            <p>Status</p>
+          </div>
+          <div className='card-contact'>
+            <div className='card-email'>
+            <span class="material-symbols-outlined">contact_mail</span>
+              <p>Email</p>
+            </div>
+            <div className='card-phone'>
+            <span class="material-symbols-outlined">call</span>
+              <p>Call</p>
+            </div>
+          </div>
+        </section>
+        <section className='card-wrapper'>
+          <div className='card-info'>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330' alt='Personal'></img>
+            <h3>First name Second name</h3>
+            <p>Work position</p>
+            <p>Status</p>
+          </div>
+          <div className='card-contact'>
+            <div className='card-email'>
+            <span class="material-symbols-outlined">contact_mail</span>
+              <p>Email</p>
+            </div>
+            <div className='card-phone'>
+            <span class="material-symbols-outlined">call</span>
+              <p>Call</p>
+            </div>
+          </div>
+        </section>
+        <section className='card-wrapper'>
+          <div className='card-info'>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330' alt='Personal'></img>
+            <h3>First name Second name</h3>
+            <p>Work position</p>
+            <p>Status</p>
+          </div>
+          <div className='card-contact'>
+            <div className='card-email'>
+            <span class="material-symbols-outlined">contact_mail</span>
+              <p>Email</p>
+            </div>
+            <div className='card-phone'>
+            <span class="material-symbols-outlined">call</span>
+              <p>Call</p>
+            </div>
+          </div>
+        </section>
+        <section className='card-wrapper'>
+          <div className='card-info'>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330' alt='Personal'></img>
+            <h3>First name Second name</h3>
+            <p>Work position</p>
+            <p>Status</p>
+          </div>
+          <div className='card-contact'>
+            <div className='card-email'>
+            <span class="material-symbols-outlined">contact_mail</span>
+              <p>Email</p>
+            </div>
+            <div className='card-phone'>
+            <span class="material-symbols-outlined">call</span>
+              <p>Call</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </body></>
   );
 }
 
 export default App;
+
