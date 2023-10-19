f9821e06c4adb649418225ad53aa1b2ae8f52f46
Maribel <marrodgar30@gmail.com>
2023-10-19 18:21:15 +0200

    ejercicio tarjetas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cards'>
+    <div className='card-group1'>
+      <div className='card'>
+        <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Jane Cooper'/>
+        <h5>Jane Cooper</h5>
+        <p>Paradigm Representative</p>
+        <p className='admin'>Admin</p>
+        <div className='botones'>
+        <button>Email</button>
+        <button>Call</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <img src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Cody Fisher'/>
+        <h5>Cody Fisher</h5>
+        <p>Lead Security</p>
+        <p className='admin'>Admin</p>
+        <div className='botones'>
+        <button>Email</button>
+        <button>Call</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <img src='https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Esther Howard'/>
+        <h5>Esther Howard</h5>
+        <p>Assuance Administradr</p>
+        <p className='admin'>Admin</p>
+        <div className='botones'>
+        <button>Email</button>
+        <button>Call</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <img src='https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Jenny Wilson'/>
+        <h5>Jenny Wilson</h5>
+        <p>Chief Accountability Analyst</p>
+        <p className='admin'>Admin</p>
+        <div className='botones'>
+        <button>Email</button>
+        <button>Call</button>
+        </div>
+      </div>
+      </div>
+      
+      <div className='card-goup2'>
+      <div className='card'>
+        <img src='https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Kristin Watson'/>
+        <h5>Kristin Watson</h5>
+        <p>Investor Data Orchestrator</p>
+        <p className='admin'>Admin</p>
+        <div className='botones'>
+        <button>Email</button>
+        <button>Call</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <img src='https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Cameron Williamson'/>
+        <h5>Cameron Williamson</h5>
+        <p>Product Infraestructure Executive</p>
+        <p className='admin'>Admin</p>
+        <div className='botones'>
+        <button>Email</button>
+        <button>Call</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <img src='https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Courtney Henry'/>
+        <h5>Courtney Henry</h5>
+        <p>Inversor Factors Associate</p>
+        <p className='admin'>Admin</p>
+        <div className='botones'>
+        <button>Email</button>
+        <button>Call</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <img src='https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Theresa Webb'/>
+        <h5>Theresa Webb</h5>
+        <p>Global Diversion Officer</p>
+        <p className='admin'>Admin</p>
+        <div className='botones'>
+        <button>Email</button>
+        <button>Call</button>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
