1921138c4d3715a2632e0fcbc8860e330819f0b3
tonebonedev <tonifcb@gmail.com>
2023-10-19 23:30:22 +0200

    toolbar first steps attempts. Frustration with the SVGs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-//import logo from "./logo.svg";
-import background-image from "./image.jpg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="wrapper">
-      <img alt="background" src="background-image" />
+    <div className="background-container">
+      <div className="toolbar-container">
+        <div className="searchbar">
+          <div className="search-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              class="u aa cq dt nz sb axp"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <span class="search-input">Search...</span>
+        </div>
+        <div className="tools">
+          <div className="tool-item">
+            <div className="icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+                class="oc se bah"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                ></path>
+              </svg>
+            </div>
+            <div className="tooltype">
+              <h4>Text</h4>
+              <p>Add freeform text with basic formatting options.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      ;
     </div>
   );
 }
