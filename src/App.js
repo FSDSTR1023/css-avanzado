955da5aeb7b16e6157a6fd9bba4c3073b81ff0ba
tonebonedev <tonifcb@gmail.com>
2023-10-19 17:46:46 +0200

    initial contact cards layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* container for all the cards */}
+      <div className="container">
+        {/* container for a card */}
+        <div className="card">
+          <div className="photo"></div>
+          <div className="name">Jane Cooper</div>
+          <div className="job-title">Paradigm Representative</div>
+          <div className="isAdmin">Admin</div>
+          {/* container for the contact details */}
+          <div className="contact-details">
+            <p>Email</p>
+            <p>Call</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="photo"></div>
+          <div className="name">Cody Fisher</div>
+          <div className="job-title"></div>
+          <div className="isAdmin">Admin</div>
+          <div className="contact-details">
+            <p>Email</p>
+            <p>Call</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="photo"></div>
+          <div className="name"></div>
+          <div className="job-title"></div>
+          <div className="isAdmin">Admin</div>
+          <div className="contact-details">
+            <p>Email</p>
+            <p>Call</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="photo"></div>
+          <div className="name"></div>
+          <div className="job-title"></div>
+          <div className="isAdmin">Admin</div>
+          <div className="contact-details">
+            <p>Email</p>
+            <p>Call</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="photo"></div>
+          <div className="name"></div>
+          <div className="job-title"></div>
+          <div className="isAdmin">Admin</div>
+          <div className="contact-details">
+            <p>Email</p>
+            <p>Call</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="photo"></div>
+          <div className="name"></div>
+          <div className="job-title"></div>
+          <div className="isAdmin"></div>
+          <div className="contact-details">
+            <p>Email</p>
+            <p>Call</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="photo"></div>
+          <div className="name"></div>
+          <div className="job-title"></div>
+          <div className="isAdmin"></div>
+          <div className="contact-details">
+            <p>Email</p>
+            <p>Call</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="photo"></div>
+          <div className="name"></div>
+          <div className="job-title"></div>
+          <div className="isAdmin"></div>
+          <div className="contact-details">
+            <p>Email</p>
+            <p>Call</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
