23d1315e58bfd19ca1e5c53900020c21b7d65af3
Juan Mencía <juamm99@gmail.com>
2023-10-19 19:21:40 +0200

    Ejercicio 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import logo from './logo.svg';
+
 import userImage from './user.png';
+
+import lapiz from './lapiz.png';
+import video from './video.png';
+import pagina from './pagina.png';
+import calendario from './calendario.png';
+import tabla from './tabla.png';
+import code from './code.png';
+
 import './App.css';
 
 function App() {
@@ -33,6 +42,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+
       {/* Ejercicio 2 */}
       <section>
         <h2>Ejercicio 2</h2>
@@ -44,8 +55,8 @@ function App() {
               <p>Cargo</p>
             </div>
             <div class="userLinks">
-              <p>Email</p>
-              <p>Call</p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Email</a></p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Call</a></p>
             </div>
           </div>
           <div class="tarjeta-ej2">
@@ -55,8 +66,8 @@ function App() {
               <p>Cargo</p>
             </div>
             <div class="userLinks">
-              <p>Email</p>
-              <p>Call</p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Email</a></p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Call</a></p>
             </div>
           </div>
           <div class="tarjeta-ej2">
@@ -66,8 +77,8 @@ function App() {
               <p>Cargo</p>
             </div>
             <div class="userLinks">
-              <p>Email</p>
-              <p>Call</p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Email</a></p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Call</a></p>
             </div>
           </div>
           <div class="tarjeta-ej2">
@@ -77,8 +88,8 @@ function App() {
               <p>Cargo</p>
             </div>
             <div class="userLinks">
-              <p>Email</p>
-              <p>Call</p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Email</a></p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Call</a></p>
             </div>
           </div>
           <div class="tarjeta-ej2">
@@ -88,8 +99,8 @@ function App() {
               <p>Cargo</p>
             </div>
             <div class="userLinks">
-              <p>Email</p>
-              <p>Call</p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Email</a></p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Call</a></p>
             </div>
           </div>
           <div class="tarjeta-ej2">
@@ -99,8 +110,8 @@ function App() {
               <p>Cargo</p>
             </div>
             <div class="userLinks">
-              <p>Email</p>
-              <p>Call</p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Email</a></p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Call</a></p>
             </div>
           </div>
           <div class="tarjeta-ej2">
@@ -110,8 +121,8 @@ function App() {
               <p>Cargo</p>
             </div>
             <div class="userLinks">
-              <p>Email</p>
-              <p>Call</p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Email</a></p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Call</a></p>
             </div>
           </div>
           <div class="tarjeta-ej2">
@@ -121,12 +132,63 @@ function App() {
               <p>Cargo</p>
             </div>
             <div class="userLinks">
-              <p>Email</p>
-              <p>Call</p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Email</a></p>
+              <p><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Call</a></p>
             </div>
           </div>
         </div>
       </section>
+
+      
+      {/* Ejercicio 3 */}
+      <section id="ejercicio3">
+        <h2 id="tituloEjercicio3">Ejercicio 3</h2>
+        <div id="toolBar">
+          <div class="tool">
+            <img src={lapiz} class="toolIcon" alt="toolIcon" />
+            <div class="toolText">
+              <p class="toolName">Text</p>
+              <p class="toolDescription">Add freeform text with basic formating options.</p>
+            </div>
+          </div>
+          <div class="tool">
+            <img src={video} class="toolIcon" alt="toolIcon" />
+            <div class="toolText">
+              <p class="toolName">Video</p>
+              <p class="toolDescription">Add a video from Youtube, Vimeo or other services.</p>
+            </div>
+          </div>
+          <div class="tool">
+            <img src={pagina} class="toolIcon" alt="toolIcon" />
+            <div class="toolText">
+              <p class="toolName">Page</p>
+              <p class="toolDescription">Add a new blank page to your project.</p>
+            </div>
+          </div>
+          <div class="tool">
+            <img src={calendario} class="toolIcon" alt="toolIcon" />
+            <div class="toolText">
+              <p class="toolName">Calendar</p>
+              <p class="toolDescription">Add a full month calendar or a week view calendar.</p>
+            </div>
+          </div>
+          <div class="tool">
+            <img src={tabla} class="toolIcon" alt="toolIcon" />
+            <div class="toolText">
+              <p class="toolName">Table</p>
+              <p class="toolDescription">Add a table for displaying larger sets of data.</p>
+            </div>
+          </div>
+          <div class="tool">
+            <img src={code} class="toolIcon" alt="toolIcon" />
+            <div class="toolText">
+              <p class="toolName">Code</p>
+              <p class="toolDescription">Add raw HTML, JavaScript or CSS code.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 }
