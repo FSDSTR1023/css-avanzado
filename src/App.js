da4c2adac462c0fdcd06cbd9ed6b240c38b92604
joelmontilla <joel.montilla.heredia@gmail.com>
2023-10-20 00:35:44 +0200

    Joel Montilla, no me salia la linea de los botones

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ function App() {
           </ul>
         </div>
         
-      </div>        
+      </div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -30,8 +31,127 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <section className="container-cartas">
+
+      <div className="card-container">
+      <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" className="imagen-card" alt="" />
+      <h1 className="text1"><b>Jane Cooper</b></h1>
+        <h2 className="text2">Paradigm Representative</h2>
+        <p className="text3">Admin</p>
+        <div className="button">
+        <a href="email" className="button1">Email</a>
+        <a href="call" className="button2">Call</a>
+        </div>
+      </div>
+
+      <div className="card-container">
+      <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" className="imagen-card" alt="" />
+      <h1 className="text1"><b>Jane Cooper</b></h1>
+      <h2 className="text2">Paradigm Representative</h2>
+        <p className="text3">Admin</p>
+
+        <div className="button">
+        <a href="email" className="button1">Email</a>
+        <a href="call" className="button2">Call</a>
+        </div>
+      </div>
+
+      <div className="card-container">
+      <img src="https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" className="imagen-card" alt="" />
+      <h1 className="text1"><b>Jane Cooper</b></h1>
+      <h2 className="text2">Paradigm Representative</h2>
+        <p className="text3">Admin</p>
+
+        <div className="button">
+        <a href="email" className="button1">Email</a>
+        <a href="call" className="button2">Call</a>
+        </div>
+      </div>
+
+      <div className="card-container">
+      <img src="https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" className="imagen-card" alt="" />
+      <h1 className="text1"><b>Jane Cooper</b></h1>
+      <h2 className="text2">Paradigm Representative</h2>
+        <p className="text3">Admin</p>
+
+        <div className="button">
+        <a href="email" className="button1">Email</a>
+        <a href="call" className="button2">Call</a>
+        </div>
+      </div>
+
+      <div className="card-container">
+      <img src="https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" className="imagen-card" alt="" />
+      <h1 className="text1"><b>Jane Cooper</b></h1>
+      <h2 className="text2">Paradigm Representative</h2>
+        <p className="text3">Admin</p>
+        <div className="button">
+        <a href="email" className="button1">Email</a>
+        <a href="call" className="button2">Call</a>
+        </div>
+      </div>
+      <div className="card-container">
+      <img src="https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" className="imagen-card" alt="" />
+      <h1 className="text1"><b>Jane Cooper</b></h1>
+      <h2 className="text2">Paradigm Representative</h2>
+        <p className="text3">Admin</p>
+
+        <div className="button">
+        <a href="email" className="button1">Email</a>
+        <a href="call" className="button2">Call</a>
+        </div>
+      </div>
+
+      <div className="card-container">
+      <img src="https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" className="imagen-card" alt="" />
+      <h1 className="text1"><b>Jane Cooper</b></h1>
+      <h2 className="text2">Paradigm Representative</h2>
+        <p className="text3">Admin</p>
+
+        <div className="button">
+        <a href="email" className="button1">Email</a>
+        <a href="call" className="button2">Call</a>
+        </div>
+      </div>
+
+      <div className="card-container">
+        <img src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" className="imagen-card" alt="" />
+        <h1 className="text1"><b>Jane Cooper</b></h1>
+        <h2 className="text2">Paradigm Representative</h2>
+        <p className="text3">Admin</p>
+
+        <div className="button">
+        <a href="email" className="button1">Email</a>
+        <a href="call" className="button2">Call</a>
+        </div>
+        
+      </div>
+      
+      </section>
+
+
+
     </div>
+    
+    
   );
+  
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
