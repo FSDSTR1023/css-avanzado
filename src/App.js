d2551b201fb803434a8372e93277908dc741dd3d
Miki <miquelxubet@gmail.com>
2023-10-20 02:42:35 +0200

    cards implementation mxubet

--- src/App.js
+++ src/App.js
@@ -14,8 +14,155 @@ function App() {
             <li>Log in</li>
           </ul>
       </header>
-      <body>
-        <p>
+    <body>
+        <div className="container-cards">
+          <div className="card">
+            <div className="perfil">
+              <img src="jane.jpg" alt="foto-jane"></img>
+              <h3>Jane Cooper</h3>
+              <p>Paradigm Representative</p>
+              <button>Admin</button>
+            </div>
+            <div id="box-mail-call">
+              <div className="email-button">
+                <img src="pictograma email.jpg" alt="icono-email"></img>
+                <p>Email</p>
+              </div>
+              <div className="call-button">
+                <img src="pictograma phone.jpg" alt="icono-telefono"></img>
+                <p>Call</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="perfil">
+              <img src="/cody.jpg" alt="foto-cody"></img>
+              <h3>Cody Fisher</h3>
+              <p>Lead Security Associate</p>
+              <button>Admin</button>
+            </div>
+            <div id="box-mail-call">
+              <div className="email-button">
+                <img src="pictograma email.jpg" alt="icono-email"></img>
+                <p>Email</p>
+              </div>
+              <div className="call-button">
+                <img src="pictograma phone.jpg" alt="icono-telefono"></img>
+                <p>Call</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="perfil">
+              <img src="Esther.jpg" alt="foto-Esther"></img>
+              <h3>Esther Foward</h3>
+              <p>Assurance Administrator</p>
+              <button>Admin</button>
+            </div>
+            <div id="box-mail-call">
+              <div className="email-button">
+                <img src="pictograma email.jpg" alt="icono-email"></img>
+                <p>Email</p>
+              </div>
+              <div className="call-button">
+                <img src="pictograma phone.jpg" alt="icono-telefono"></img>
+                <p>Call</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="perfil">
+              <img src="jenny.jpg" alt="foto-jenny"></img>
+              <h3>Jenny Wilson</h3>
+              <p>Chief Accountability Analyst</p>
+              <button>Admin</button>
+            </div>
+            <div id="box-mail-call">
+              <div className="email-button">
+                <img src="pictograma email.jpg" alt="icono-email"></img>
+                <p>Email</p>
+              </div>
+              <div className="call-button">
+                <img src="pictograma phone.jpg" alt="icono-telefono"></img>
+                <p>Call</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="perfil">
+              <img src="kristin.jpeg" alt="foto-kristin"></img>
+              <h3>Kristin Watson</h3>
+              <p>Investor Data Orchestrator</p>
+              <button>Admin</button>
+            </div>
+            <div id="box-mail-call">
+              <div className="email-button">
+                <img src="pictograma email.jpg" alt="icono-email"></img>
+                <p>Email</p>
+              </div>
+              <div className="call-button">
+                <img src="pictograma phone.jpg" alt="icono-telefono"></img>
+                <p>Call</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="perfil">
+              <img src="cameron.jpg" alt="foto-Cameron"></img>
+              <h3>Cameron Williamson</h3>
+              <p>Product Infrastructure Executive</p>
+              <button>Admin</button>
+            </div>
+            <div id="box-mail-call">
+              <div className="email-button">
+                <img src="pictograma email.jpg" alt="icono-email"></img>
+                <p>Email</p>
+              </div>
+              <div className="call-button">
+                <img src="pictograma phone.jpg" alt="icono-telefono"></img>
+                <p>Call</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="perfil">
+              <img src="courtney.jpg" alt="foto-Courtney"></img>
+              <h3>Courtney Henry</h3>
+              <p>Investor Factors Associate</p>
+              <button>Admin</button>
+            </div>
+            <div id="box-mail-call">
+              <div className="email-button">
+                <img src="pictograma email.jpg" alt="icono-email"></img>
+                <p>Email</p>
+              </div>
+              <div className="call-button">
+                <img src="pictograma phone.jpg" alt="icono-telefono"></img>
+                <p>Call</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="perfil">
+              <img src="theresa.jpg" alt="foto-Theresa"></img>
+              <h3>Theresa Webb</h3>
+              <p>Global Division Officer</p>
+              <button>Admin</button>
+            </div>
+            <div id="box-mail-call">
+              <div className="email-button">
+                <img src="pictograma email.jpg" alt="icono-email"></img>
+                <p>Email</p>
+              </div>
+              <div className="call-button">
+                <img src="pictograma phone.jpg" alt="icono-telefono"></img>
+                <p>Call</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <p id="info">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -25,7 +172,7 @@ function App() {
           rel="noopener noreferrer">
           Learn React
         </a>
-      </body>
+    </body>
     </div>
   );
 }
