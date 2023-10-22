cfc5f088e624580031439e68da1a5eca9299130f
Noelia <omnoelia@hotmail.com>
2023-10-22 20:26:14 +0200

    ejercicio-tarjetas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
 import logo from './logo.svg';
 import './App.css';
+function App(){
+    return(
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="App">
+
+
+    <div class="contact-cards">
+        <h2>Contact Cards</h2>
     </div>
-  );
-}
 
+
+    <div className="contenedor-padre">
+
+
+    <div class="card">
+        <div class="informacion-personal">
+        <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"alt="Jane-cooper"/>
+        <h2>Jane Cooper</h2>
+        <p>Paradigm Representative</p>
+        <button className="boton">Admin</button>
+        </div>
+        <div class="formulario">
+            <h2>✉️Email</h2>
+            <h2>📞Call</h2>
+        </div>
+    </div>
+    <div class="card">
+    <div class="informacion-personal">
+     <img src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"alt="Cody Fisher"/>
+     <h2>Cody Fisher</h2>
+     <p>Lead Security Associate</p>
+     <button className="boton">Admin</button>
+     </div>
+     <div class="formulario">
+            <h2>✉️Email</h2>
+            <h2>📞Call</h2>
+        </div>
+    </div>
+    <div class="card">
+    <div class="informacion-personal">
+        <img src="https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"alt="Esther Howard"/>
+        <h2>Esther Howard</h2>
+        <p>Assurance Administrator</p>
+        <button className="boton">Admin</button>
+        </div>
+        <div class="formulario">
+            <h2>✉️Email</h2>
+            <h2>📞Call</h2>
+        </div>
+    </div>
+    <div class="card">
+    <div class="informacion-personal">
+        <img src="https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"alt="Jenny Wilson"/>
+        <h2>Jenny Wilson</h2>
+        <p>Chief Accountability Analyst</p>
+        <button className="boton">Admin</button>
+        </div>
+        <div class="formulario">
+            <h2>✉️Email</h2>
+            <h2>📞Call</h2>
+        </div>
+    </div>
+
+
+    <div class="card">
+    <div class="informacion-personal">
+        <img src="https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"alt="Kristin Watson"/>
+        <h2>Kristin Watson</h2>
+        <p>Investor Data Orchestrator</p>
+        <button className="boton">Admin</button>
+        </div>
+        <div class="formulario">
+            <h2>✉️Email</h2>
+            <h2>📞Call</h2>
+        </div>
+    </div>
+
+
+    <div class="card">
+    <div class="informacion-personal">
+        <img src="https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="Cameron Williamson"/>
+        <h2>Cameron Williamson</h2>
+        <p>Product Infrastructure Executive</p>
+        <button className="boton">Admin</button>
+        </div>
+        <div class="formulario">
+            <h2>✉️Email</h2>
+            <h2>📞Call</h2>
+        </div>
+    </div>
+
+
+    <div class="card">
+    <div class="informacion-personal">
+        <img src="https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="Courtney Henry"/>
+        <h2>Courtney Henry</h2>
+        <p>Investor Factors Associate</p>
+        <button className="boton">Admin</button>
+        </div>
+        <div class="formulario">
+            <h2>✉️Email</h2>
+            <h2>📞Call</h2>
+        </div>
+    </div>
+
+
+    <div class="card">
+    <div class="informacion-personal">
+        <img src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="Theresa Webb"/>
+        <h2>Theresa Webb</h2>
+        <p>Global Division Officer</p>
+        <button className="boton">Admin</button>
+        </div>
+        <div class="formulario">
+            <h2>✉️Email</h2>
+            <h2>📞Call</h2>
+        </div>
+    </div>
+
+
+    </div>
+    </div>
+
+
+    );
+}
 export default App;
