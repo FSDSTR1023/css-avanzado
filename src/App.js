ce44173809049837ae0aa9ce578631ffa6dd475f
Alejopizarro <alejomkt10@gmail.com>
2023-10-23 15:22:13 +0200

    ejercicios flex y grid Alejopizarro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
-import './ App.css';
+import noticia from './fondo-fullstack.jpg';
+import avatar from './avatar.png';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <nav>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="nav-items">
+      <header>
+      <nav>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div class="nav-items">
         <ul>
           <li>Products</li>
           <li>Features</li>
@@ -15,20 +17,136 @@ function App() {
           <li>Company</li>
           <li>Log In</li>
         </ul>
-        </div>
-        </nav>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </div>
+    </nav>
       </header>
+      <div class="card-wrapper">
+      <div class="card">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <h4>Jane Cooper</h4>
+        <p>Paradigm Representative</p>
+        <p class="admin">admin</p>
+        <div class="botones">
+          <button>📩Email</button>
+          <button>📞Call</button>
+        </div>
+      </div>
+      <div class="card">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <h4>Cody Fisher</h4>
+        <p>Lead Security Associate</p>
+        <p class="admin">admin</p>
+        <div class="botones">
+          <button>📩Email</button>
+          <button>📞Call</button>
+        </div>
+      </div>
+      <div class="card">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <h4>Esther Howard</h4>
+        <p>Assurance Administrator</p>
+        <p class="admin">admin</p>
+        <div class="botones">
+          <button>📩Email</button>
+          <button>📞Call</button>
+        </div>
+      </div>
+      <div class="card">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <h4>Jenny Wilson</h4>
+        <p>Chief Accountability Analyst</p>
+        <p class="admin">admin</p>
+        <div class="botones">
+          <button>📩Email</button>
+          <button>📞Call</button>
+        </div>
+      </div>
+      <div class="card">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <h4>Kristin Watson</h4>
+        <p>Investor Data Orchestrator</p>
+        <p class="admin">admin</p>
+        <div class="botones">
+          <button>📩Email</button>
+          <button>📞Call</button>
+        </div>
+      </div>
+      <div class="card">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <h4>Cameron Williamson</h4>
+        <p>Product Infraestructure Executive</p>
+        <p class="admin">admin</p>
+        <div class="botones">
+          <button>📩Email</button>
+          <button>📞Call</button>
+        </div>
+      </div>
+      <div class="card">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <h4>Courtney Henry</h4>
+        <p>Investor Factors Associate</p>
+        <p class="admin">admin</p>
+        <div class="botones">
+          <button>📩Email</button>
+          <button>📞Call</button>
+        </div>
+      </div>
+      <div class="card">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <h4>Theresa Webb</h4>
+        <p>Global Division Officer</p>
+        <p class="admin">admin</p>
+        <div class="botones">
+          <button>📩Email</button>
+          <button>📞Call</button>
+        </div>
+      </div>
+    </div>
+    <container class="container">
+      <div class="title">
+        <h2>From the blog</h2>
+        <p>Learn how te grow your business with our expert device</p>
+      </div>
+      <img src={noticia} className="image1" alt="noticia" />
+      <div class="notice1">
+        <p class="fecha">mar 14, 2023</p>
+        <h4>Boost your conversion rate</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div class="autor1">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <div class="nombreAutor1">
+          <h5>Michael Foster</h5>
+          <p>Co-Founder & CTO</p>
+        </div>
+      </div>
+      <img src={noticia} className="image2" alt="noticia" />
+      <div class="notice2">
+        <p class="fecha">mar 14, 2023</p>
+        <h4>How to use search engine optimization to drive seals</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div class="autor2">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <div class="nombreAutor2">
+          <h6>Lindsey Walton</h6>
+          <p>Front-end Developer</p>
+        </div>
+      </div>
+      <img src={noticia} className="image3" alt="noticia" />
+      <div class="notice3">
+        <p class="fecha">mar 14, 2023</p>
+        <h4>Boost your conversion rate</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div class="autor3">
+      <img src={avatar} className="avatar" alt="avatar" />
+        <div class="nombreAutor3">
+          <h6>Michael Foster</h6>
+          <p>Co-Founder & CTO</p>
+        </div>
+      </div>
+    </container>
     </div>
   );
 }
