571621a2f6181224b3f98db50ca59f0aeb9944f6
tonebonedev <tonifcb@gmail.com>
2023-10-24 21:46:23 +0200

    blog exercise

--- src/App.js
+++ src/App.js
@@ -3,30 +3,108 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav className="navbar">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="nav-items">
-            <p className="item">Product</p>
-            <p className="item">Features</p>
-            <p className="item">Marketplace</p>
-            <p className="item">Company</p>
-            <p className="item">Log in</p>
+    <div className="container">
+      <section className="app-header">
+        <header className="header">
+          <h2>From the blog</h2>
+          <h3>Learn how to grow your business with our expert advice.</h3>
+        </header>
+        <div className="card-container">
+          <div className="card">
+            <div className="image-container">
+              <img
+                className="study"
+                alt="study"
+                src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"
+              />
+            </div>
+            <div className="info">
+              <time>Mar 16, 2020</time>
+              <p>Marketing</p>
+            </div>
+            <h4>Boost your conversion rate</h4>
+            <p className="infoP">
+              Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam
+              vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+              exercitationem placeat consectetur nulla deserunt vel. Iusto
+              corrupti dicta.
+            </p>
+            <div className="person">
+              <img
+                className="person-photo"
+                src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt="michael"
+              />
+              <div className="person-info">
+                <h6>Michael Foster</h6>
+                <p>Co-Founder / CTO</p>
+              </div>
+            </div>
           </div>
-        </nav>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="card">
+            <div className="image-container">
+              <img
+                className="study"
+                alt="study"
+                src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"
+              />
+            </div>
+            <div className="info">
+              <time>Mar 16, 2020</time>
+              <p>Marketing</p>
+            </div>
+            <h4>Boost your conversion rate</h4>
+            <p className="infoP">
+              Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam
+              vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+              exercitationem placeat consectetur nulla deserunt vel. Iusto
+              corrupti dicta.
+            </p>
+            <div className="person">
+              <img
+                className="person-photo"
+                src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt="michael"
+              />
+              <div className="person-info">
+                <h6>Michael Foster</h6>
+                <p>Co-Founder / CTO</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="image-container">
+              <img
+                className="study"
+                alt="study"
+                src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"
+              />
+            </div>
+            <div className="info">
+              <time>Mar 16, 2020</time>
+              <p>Marketing</p>
+            </div>
+            <h4>Boost your conversion rate</h4>
+            <p className="infoP">
+              Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam
+              vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+              exercitationem placeat consectetur nulla deserunt vel. Iusto
+              corrupti dicta.
+            </p>
+            <div className="person">
+              <img
+                className="person-photo"
+                src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt="michael"
+              />
+              <div className="person-info">
+                <h6>Michael Foster</h6>
+                <p>Co-Founder / CTO</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
