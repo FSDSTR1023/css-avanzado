0d4b15d8ace859c8160cdfee113ceaa5f17ecf1c
alexander-lh <lasherasalexander@gmail.com>
2023-10-31 21:58:17 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -28,11 +28,11 @@ function App() {
           </div>
           <div className='card-contact'>
             <div className='card-email'>
-            <span class="material-symbols-outlined">contact_mail</span>
+            <span className="material-symbols-outlined">contact_mail</span>
               <p>Email</p>
             </div>
             <div className='card-phone'>
-            <span class="material-symbols-outlined">call</span>
+            <span className="material-symbols-outlined">call</span>
               <p>Call</p>
             </div>
           </div>
@@ -46,11 +46,11 @@ function App() {
           </div>
           <div className='card-contact'>
             <div className='card-email'>
-            <span class="material-symbols-outlined">contact_mail</span>
+            <span className="material-symbols-outlined">contact_mail</span>
               <p>Email</p>
             </div>
             <div className='card-phone'>
-            <span class="material-symbols-outlined">call</span>
+            <span className="material-symbols-outlined">call</span>
               <p>Call</p>
             </div>
           </div>
@@ -64,11 +64,11 @@ function App() {
           </div>
           <div className='card-contact'>
             <div className='card-email'>
-            <span class="material-symbols-outlined">contact_mail</span>
+            <span className="material-symbols-outlined">contact_mail</span>
               <p>Email</p>
             </div>
             <div className='card-phone'>
-            <span class="material-symbols-outlined">call</span>
+            <span className="material-symbols-outlined">call</span>
               <p>Call</p>
             </div>
           </div>
@@ -82,11 +82,11 @@ function App() {
           </div>
           <div className='card-contact'>
             <div className='card-email'>
-            <span class="material-symbols-outlined">contact_mail</span>
+            <span className="material-symbols-outlined">contact_mail</span>
               <p>Email</p>
             </div>
             <div className='card-phone'>
-            <span class="material-symbols-outlined">call</span>
+            <span className="material-symbols-outlined">call</span>
               <p>Call</p>
             </div>
           </div>
@@ -100,11 +100,11 @@ function App() {
           </div>
           <div className='card-contact'>
             <div className='card-email'>
-            <span class="material-symbols-outlined">contact_mail</span>
+            <span className="material-symbols-outlined">contact_mail</span>
               <p>Email</p>
             </div>
             <div className='card-phone'>
-            <span class="material-symbols-outlined">call</span>
+            <span className="material-symbols-outlined">call</span>
               <p>Call</p>
             </div>
           </div>
@@ -118,11 +118,11 @@ function App() {
           </div>
           <div className='card-contact'>
             <div className='card-email'>
-            <span class="material-symbols-outlined">contact_mail</span>
+            <span className="material-symbols-outlined">contact_mail</span>
               <p>Email</p>
             </div>
             <div className='card-phone'>
-            <span class="material-symbols-outlined">call</span>
+            <span className="material-symbols-outlined">call</span>
               <p>Call</p>
             </div>
           </div>
@@ -136,11 +136,11 @@ function App() {
           </div>
           <div className='card-contact'>
             <div className='card-email'>
-            <span class="material-symbols-outlined">contact_mail</span>
+            <span className="material-symbols-outlined">contact_mail</span>
               <p>Email</p>
             </div>
             <div className='card-phone'>
-            <span class="material-symbols-outlined">call</span>
+            <span className="material-symbols-outlined">call</span>
               <p>Call</p>
             </div>
           </div>
@@ -154,16 +154,72 @@ function App() {
           </div>
           <div className='card-contact'>
             <div className='card-email'>
-            <span class="material-symbols-outlined">contact_mail</span>
+            <span className="material-symbols-outlined">contact_mail</span>
               <p>Email</p>
             </div>
             <div className='card-phone'>
-            <span class="material-symbols-outlined">call</span>
+            <span className="material-symbols-outlined">call</span>
               <p>Call</p>
             </div>
           </div>
         </section>
       </main>
+      <article className='layout'>
+        <div className='layout-head'>
+          <h2>From the blog</h2>
+          <p>Learn how to grow your business with our expert advice.</p>
+        </div>
+        <div className='layout-card'>
+          <div className='layout-card-info'>
+            <img src='https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80' alt='Desktop'></img>
+            <div className='layout-card-date'>
+              <time dateTime='2023-10-18'>18 oct. 2023</time>
+              <a href='#'>Marketing</a>
+            </div>
+            <div>
+              <h3>Boost your conversion rate</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div className='layout-card-person'>
+              <img src='https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'  className='writer-img'></img>
+              <p className='writer-name'>Michael Foster</p>
+              <p className='writer-job'>Co-Founder / CTO</p>
+            </div>
+          </div>
+          <div className='layout-card-info'>
+            <img src='https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80' alt='Desktop'></img>
+            <div className='layout-card-date'>
+              <time dateTime='2023-10-19'>19 oct. 2023</time>
+              <a href='#'>Sales</a>
+            </div>
+            <div>
+              <h3>Boost your conversion rate</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div className='layout-card-person'>
+              <img src='https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' className='writer-img'></img>
+              <p className='writer-name'>Michael Foster</p>
+              <p className='writer-job'>Co-Founder / CTO</p>
+            </div>
+          </div>
+          <div className='layout-card-info'>
+            <img src='https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80' alt='Desktop'></img>
+            <div className='layout-card-date'>
+              <time dateTime='2023-10-20'>20 oct. 2023</time>
+              <a href='#'>Bussines</a>
+            </div>
+            <div>
+              <h3>Boost your conversion rate</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div className='layout-card-person'>
+              <img src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'  className='writer-img'></img>
+              <p className='writer-name'>Michael Foster</p>
+              <p className='writer-job'>Co-Founder / CTO</p>
+            </div>
+          </div>
+        </div>        
+      </article>
     </body></>
   );
 }
