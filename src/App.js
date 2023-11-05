6cbaa16508f7039fa6d32d5dd839f55c25e63f97
Enrique Garcia <enrique@amigosdelcara.com>
2023-11-05 13:13:42 +0100

    ft exercice 3 done

--- src/App.js
+++ src/App.js
@@ -222,9 +222,67 @@ function App() {
               </div>
             </div>
           </div>
+          <hr width="100%"></hr>
       </main>
       <footer className="App-footer">
-
+        <div className="footerbox">
+          <div className="busqueda">
+            <img className="iconobusqueda" alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABI0lEQVR4nO2WwUrDQBCGv8SAXtJTQdtcjL21N+vZEhNBiOL7v40M/AuDl2Y3S1TsD3PZzMyX7MxOFi76RSqBBjgCPfAu67XWyCerNsAAfJ6xQb6zVQAHl7gDHoAauJLVWuuc316xyToo0QdwfyaZPWvlG+BJ2jjoOiJu7eDR2166mtqXxqp1NY9quMbVNKVWhav5NibwSUHWNKnaKYcdtcnqFWQdm6paOSzXZI0KqmaAK+UY/wS4z7DVK9fZk3XM2FyPSx+nl5TjVLoBYsNgzgApfmJk3pGovYO3kT+JN+A6FVw4eKj5Tt1eyVZaCzUN0OCfDI+5CLwCt4J1ueClOjRcfUZdfQYdme23UmSFx8rD7WVv+I/w05LgAD8Bz0uDL8L0BaKkKFjF3DttAAAAAElFTkSuQmCC"></img>
+            <input id="headlessui-combobox-input-3" role="combobox" type="text" aria-expanded="true" aria-autocomplete="list" data-headlessui-state="open" value="Search..." aria-controls="headlessui-combobox-options-4"></input>
+            <hr width="100%"></hr>
+          </div>
+          <ul className="listabusqueda" role="listbox" id="headlessui-combobox-options-4" data-headlessui-state="">
+          <li className="elementolista" id="headlessui-combobox-option-5" role="option" tabindex="-1" aria-selected="false" data-headlessui-state="">
+            <img className="iconos" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABcklEQVR4nGMIrf/PFl3zszOm5uezmJpf/2mKq389ja7+2QGykwHEoLmFNagYZCcDXXxa8+t/QM7t/yLSZv/ZOAT+m3lO/sBAL1/6pF/4z8Et9p+BgeE/G4fgfwyLHSM2/OfilQIrIBezsvP9d084BPcpyFKI5efBlovKWGBazMUnTVVLeQTkwZb5pJ+H+xwkjmExtS2FyXGALb+AsIdaFrOwcv93i9sLNiMw9+5/HkFFFHluftn/Adk3qWsxqZbGUMNiciyNwWcxqdkFbKmAAlGWxlDLYlBC4uaXQ7EUlLBA4iB5WIKjqsWEfAqyFJTaqWoxIZ+CMMhSbOaRbTFOn+bcIso8BnItVjFMJmgpTSwWk7UmaClNLOYTVv8vJmf7X9Uo5X9g7h2c6qhucQyReNTimNGgjhnoxMVFYdMHG+bikyFssVPEJqpaDrLUKXIzYYvphRlA3Qp6Wxpd8+vJgHRhYmp+tjOAO20gy+nhc6ROGwAA5xVeZDmdXwAAAABJRU5ErkJggg==" alt=""></img>
+            <div className="textobusqueda">
+              <span>Text</span>
+              <span className="fuentegris">Add freeform text with basic formatting options.</span>
+            </div>
+          </li>
+          <li className="elementolista" id="headlessui-combobox-option-5" role="option" tabindex="-1" aria-selected="false" data-headlessui-state="">
+          <img className="iconos" alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABM0lEQVR4nGNw3/BD3XPdz6Ne637+9lr/6z9V8DqQWT+PeK77ocYANpxaBq9Hxz8PM1DV5esxfcJAO9f/AuNhZIHLouf/xYy9/jOxsP1nYGAgiFm4+P4r+uWDwpk4CxS8c8CGg2jloFKCWNI6BGyRccUauGGea779Nyxd/t+iZS+mBcK6Dv8FNa2J9rrn2u9gC1Qj6/67LXv7XzOp5z+nqBxYTFjXEYsFOvZgTFL4MjD855XX+c/KLQBmC2nb/ucSV0Qxh2ILGBmZ/ouZeP+37DiE1RyKLVAJqUQRG3oWMNI6iHiJjmRqJlM9JywZzScXnNGUAkv+q8e2EsRSdhE4M5pV52EcRYWJN4lFRQHxRQWtMMMwsGAdzavMn0doZYHnup+HGEBNC1DtT+1mC8hw7zU/VAFKPemV1MJEAgAAAABJRU5ErkJggg=="></img>
+            <div className="textobusqueda">
+              <span>Video</span>
+              <span className="fuentegris">Add a video fro Youtube, Vimeo or other services.</span>
+            </div>
+          </li>
+          <li className="elementolista" id="headlessui-combobox-option-5" role="option" tabindex="-1" aria-selected="false" data-headlessui-state="">
+            <img className="iconos" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAvklEQVR4nGO4ovWf7YXu//YXev8fP9f9/4ca+IXu/0cvdP+3gcxmADGoZfBzTIvaGNBdvlRh219JVpn/DAwMJGEpVtn/IL1oljxhQLcVpJBUwxmQLEE3D8MCmGJSg4MBh75RC/6MBtGf0VTEMDIzGgOe8oimFhSL1/2lmQXFWAynmgXFOAynigXFeAzHagEpFU6RWN0/gqkLVEEjC4CqPWIsKSLC8Be6/x/StNJ/rve/lQHabGlD9wklGLnZAgB1j8jD3v1H6AAAAABJRU5ErkJggg==" alt=""></img>
+            <div className="textobusqueda">
+              <span>Page</span>
+              <span className="fuentegris">Add a new blank page to your project.</span>
+            </div>
+          </li>
+          <li className="elementolista" id="headlessui-combobox-option-5" role="option" tabindex="-1" aria-selected="false" data-headlessui-state="">
+          <img className="iconos" alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAArUlEQVR4nGPQurKKTf7ktE6FE9OeKZyc/p9K+Kn8iekdILMZQAxsihgYGMBYgYBh+NSBzGbA5XJqWKBwYvpzBmQB8f7s/yxignBNpGKQXvH+HFQHIHMoMZwBZom4IG4LiA0WBRKCa2AsYKAQKwy4BQpDPg4URi04ORpEJ0dsKmKhdXEt3p8DVkCJ4eITMCucp+QGiQJBPO0JzkqfKvjE9HYGcLMFYgk1fQJvtgAA3TulwzX7exgAAAAASUVORK5CYII="></img>
+            <div className="textobusqueda">
+              <span>Calendar</span>
+              <span className="fuentegris">Add a full month calendar or a week view calendar.</span>
+            </div>
+          </li>
+          <li className="elementolista" id="headlessui-combobox-option-5" role="option" tabindex="-1" aria-selected="false" data-headlessui-state="">
+            <img className="iconos" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAyElEQVR4nO2XzQ3CMAyF3z03OMEUwBSdA8WMwEJNmKNkFZcd6CkorVpVrYBLfkDy4V0sy19iOY4NALjUjxNZdtrwkyz7FNJDbHe+tUdMUMNdKuBKhrseHk6RDWqn2zdImd5PacfSWF1rrzY7DyCK1Hbfx6QFZwUOjrGgczh9A4/OKW0kYJqlJrbo58EkxYVMzwlSXJDOBSkuL52L/uI/VqVGnyoMexHhb4c9XXC8dQXADcI6kXuF0aY99PtTgGvL99RLW7jpCH0BfS6qGuheeN0AAAAASUVORK5CYII=" alt=""></img>
+            <div className="textobusqueda">
+              <span>Table</span>
+              <span className="fuentegris">Add a table for displaying larger sets of data.</span>
+            </div>
+          </li>
+          <li className="elementolista" id="headlessui-combobox-option-5" role="option" tabindex="-1" aria-selected="false" data-headlessui-state="">
+          <img className="iconos" alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAABrUlEQVR4nGNgYGBgOJX0RP1M6uPpp1MfHTqd+ug4jfAhkB0XUp6oMcAsPZ36mJYWouHHh8CWQ316nJ74TMqjaQw0Dl6cwc4A47Q4TbkuyCH8k4GB4T8tsCCn8M82p6nXYfbBLQZJ0MpSGBbiFPmJYTGtLWWA4lGLjw94UJ+mURZiGPIWn0p9eFxLVO+jqpDWJ5pYfDjxxkllQfXPaiI6KBYsCdp2AaRegEMInjfVhLQ/qQhqfAbpodjiIM3opyBxOX6lr8ji2aYV90Di1rKOb2BicnyKX0FiQZoxTymyeLrX8stMjEz/GBkZ/0/yXHwFWc5K1uENSH2uaeU9YtQzEGvxkcRbJ6V55b/h8gEoiEFyS4O2X8AWQpI8st+Qg5yBPIujUSxeHACLX8Ff6A4K1IiizOLTeIIu26QcEr9yzq+xqQfh6V4rL1MncfEpfsUXv1RNXKeh2QmURUBZBRG/gr+wxS8oT4MwVbLTaSxYW9Tgo7ao/kdi1FLV4tNkYIZBazHDSGiBHKJV8OLBhwauQX8h5Ykavbsw59Ifq4L7TyDLT6U+on2nLeXRNJilAGotoX206accAAAAAElFTkSuQmCC"></img>
+            <div className="textobusqueda">
+              <span>Code</span>
+              <span className="fuentegris">Add raw HTML, Javascript or CSS code.</span>
+            </div>
+          </li>
+          <li className="elementolista" id="headlessui-combobox-option-5" role="option" tabindex="-1" aria-selected="false" data-headlessui-state="">
+          <img className="iconos" alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABJUlEQVR4nGP4b2zM+l/XOvu/rvXm/3o2x6mCda03/9e1yQKZzQBmUMtgPXSLbLIYqOpyPQy8hYGGhh8HYbgFK+U0rouxsP5kYGD4TwkWZ2H9CTILwwJxKhjOgGQJhgUwSYqDhAHVnMFhgT4H9ydDDu5PNLPAkIP7kxEnDS34T+84eK5pdtqZW+DtR23LkxRbsFtR5zIy/7OO5Uk9Du5PILWB/MKvKLJgjZzGNSYGxn/pQhJPQfw/ejbHXXkE3iKn+X5JxTtkWXBdzegcHzPzb5h4vojk4wRB8WfomYqTienvKRX9CyRZ8EXH6qQmO+cXYnOuEhvHN1h8DI1U9J8cC2he2K2U07guTsvi+j+NMANNq0xdm020rfT1bDIZIM0WmyxaNVsAZygn0L3q2cEAAAAASUVORK5CYII="></img>
+            <div className="textobusqueda">
+              <span>Image</span>
+              <span className="fuentegris">Add a simple image or a photo gallery.</span>
+            </div>
+          </li>
+          </ul>
+        </div>
       </footer>
     </div>
   );
