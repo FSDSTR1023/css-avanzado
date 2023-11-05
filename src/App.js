9a4a9473bc54f51ee76f973742eed8460458a98b
Angelheredia20 <angelheredia20@gmail.com>
2023-11-05 13:46:16 +0100

    navigation-bar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav className='nav-wrapper'>
-        <img src={logo} className="App-logo" alt="logo" />
-          <ul className='nav-item'>
-            <li><a href="#">Product</a></li>
-            <li><a href="#">Features</a></li>
-            <li><a href="#">Marketplace</a></li>
-            <li><a href="#">Company</a></li>
-            <li><a href="#">Log in</a></li>
-          </ul>
-       </nav>
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <section>
+    <div>
+ <header className="nav">
+  
+    <img src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="logo" />
+    <ul className='box-nav'> 
+      <li><a href="https://tailwindui.com/components/application-ui/lists/grid-lists">Product</a></li> 
+      <li className='button'>Features</li>
+      <li>Marketplace</li>
+      <li>Company</li>
+      <li>Login</li>
+    </ul>      
+  
+ </header>
 
-export default App;
+ <div className='card-container'>
+   
+  <div className='card'>
+    
+    <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Jane Cooper'/>
+    <h5 className='titulo'>Jane Cooper</h5>
+    <p className='texto-persona'>Regional Paradigm Technician</p>
+    <p className='admin'>Admin</p>
+    <div className='button'><button>Email</button>
+    <button>Call</button></div>
+  </div>
+
+  <div className='card'>
+    <img src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Cody Fisher'/>
+    <h5 className='titulo'>Cody Fisher</h5>
+    <p className='texto-persona'>Products Directives Officer</p>
+    <p className='admin'>Admin</p>
+    <div className='button'><button>Email</button>
+    <button>Call</button></div>
+  </div>
+  <div className='card'>
+    <img src='https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Esther Howard'/>
+    <h5 className='titulo'>Esther Howard</h5>
+    <p className='texto-persona'>Forward Response Developer</p>
+    <p className='admin'>Admin</p>
+    <div className='button'><button>Email</button>
+    <button>Call</button></div>
+  </div>
+  <div className='card'>
+    <img src='https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Jenny Wilson'/>
+    <h5 className='titulo'>Jenny Wilson</h5>
+    <p>Central Security Manager</p>
+    <p className='admin'>Admin</p>
+    <div className='button'><button>Email</button>
+    <button>Call</button></div>
+  </div>
+
+  <div className='card'>
+    <img src='https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Kristin Watson'/>
+    <h5 className='titulo'>Kristin Watson</h5>
+    <p>Lead Implementation Liaison</p>
+    <p className='admin'>Admin</p>
+    <div className='button'><button>Email</button>
+    <button>Call</button></div>
+  </div>
+  <div className='card'>
+    <img src='https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Cameron Williamson'/>
+    <h5 className='titulo'>Cameron Williamson</h5>
+    <p>Internal Applications Engineer</p>
+    <p className='admin'>Admin</p>
+    <div className='button'><button>Email</button>
+    <button>Call</button></div>
+  </div>
+  <div className='card'>
+    <img src='https://images.unsplash.com/photo-1534751516642-a1af1ef26a56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Courtney Henry'/>
+    <h5 className='titulo'>Courtney Henry</h5>
+    <p className='texto-persona'>Investor Factors Associate</p>
+    <p className='admin'>Admin</p>
+    <div className='button'><button>Email</button>
+    <button>Call</button></div>
+  </div>
+  <div className='card'>
+    <img src='https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60' alt='Theresa Webb'/>
+    <h5 className='titulo'>Theresa Webb</h5>
+    <p className='texto-persona'>Global Division Officer</p>
+    <p className='admin'>Admin</p>
+    <div className='button'><button>Email</button>
+    <button>Call</button></div>
+  </div>
+
+</div>
+ </div> 
+</section>
+ );
+ }
+export default App;
